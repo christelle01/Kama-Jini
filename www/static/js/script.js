0b7940071f817ea9de89ca0438e9bb3472a3abde
christelle01 <christellecarmela@gmail.com>
2022-08-26 15:15:16 +0000

    add projects card

--- www/static/js/script.js
+++ www/static/js/script.js
@@ -310,3 +310,39 @@ jQuery(function ($) { "use strict";
 
 
 	
+
+
+
+
+
+	$('.card').matchHeight();
+$(function(){
+  
+  $('#nav-all').addClass('active');
+  
+  $('ul.nav.nav-pills li a').click(function() {           
+    $(this).parent().addClass('active').siblings().removeClass('active');  
+});
+   
+  
+   $('#nav-all').click(function(){
+     $('.col-md-4').show();
+   });
+  
+   $('#nav-css').click(function(){
+     $('.col-md-4').hide();
+     $('.cssProject').show();
+   });
+  
+   $('#nav-js').click(function(){
+     $('.col-md-4').hide();
+     $('.jsProject').show();
+   })
+  
+
+  
+ 
+  
+});
+//cssProject
+//javascriptProject
